8f34f14ed86d03cbcd54a1cbe88e4f0c2c2f0e98
RITISH <110611730+Ritish-02@users.noreply.github.com>
2023-08-09 20:12:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -18,6 +18,3 @@ def predict():
     output = round(prediction[0], 1)
 
     return render_template('index.html', prediction_text='Your Rating is: {}'.format(output))
-
-if __name__ == "__main__":
-    app.run(debug=True)
